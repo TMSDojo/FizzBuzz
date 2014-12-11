ffdcfb3df3e0795e19d886f932582817dea68a84
Magnus <magnusb@tmsoftware.is>
2014-12-11 21:29:43 +0000

    Initial

---
+++ FizzBuzz/BizzBuzz.cs
@@ -0,0 +1,46 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzzImpl : IFizzBuzzKata
+    {
+       string IFizzBuzzKata.Answer(int number)
+       {
+          return number.Fizz()
+                       .Buzz(number)
+                       .OrNumber(number);
+       }
+    }
+
+    public static class Ext
+    {
+       public static string Fizz(this int number)
+       {
+          return (number%3).Equals(0)
+             ? "fizz"
+             : "";
+       }
+
+       public static string Buzz(this string s,int number)
+       {
+          return (number % 5).Equals(0)
+             ? s + "buzz"
+             : s;
+       }
+
+       public static string OrNumber(this string s, int number)
+       {
+          return string.IsNullOrEmpty(s)
+             ? number.ToString()
+             : s;
+       }
+    }
+
+    public interface IFizzBuzzKata
+    {
+       /// <summary>
+       /// Give an answer to the current game
+       /// </summary>
+       /// <param name="number">current number in the game sequence</param>
+       /// <returns>appropriate answer to the current number</returns>
+       string Answer(int number);
+    }
+}
---
+++ FizzBuzz/Tests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace FizzBuzz
+{
+   [TestFixture]
+   public class Tests
+   {
+      private IFizzBuzzKata target;
+
+      [SetUp]
+      public void Setup()
+      {
+         target = new FizzBuzzImpl();
+      }
+
+      [Test]
+      public void one_should_be_1()
+      {
+         target.Answer(1).Should().Be("1");
+      }
+
+      [Test]
+      public void two_should_be_2()
+      {
+         target.Answer(2).Should().Be("2");
+      }
+
+      [Test]
+      public void four_should_be_4()
+      {
+         target.Answer(4).Should().Be("4");
+      }
+
+      [Test]
+      public void three_should_be_fizz()
+      {
+         target.Answer(3).Should().Be("fizz");
+      }
+
+      [Test]
+      public void five_should_be_buzz()
+      {
+         target.Answer(5).Should().Be("buzz");
+      }
+
+      [Test]
+      public void nine_should_be_fizz()
+      {
+         target.Answer(9).Should().Be("fizz");
+      }
+
+      [Test]
+      public void ten_should_be_fizz()
+      {
+         target.Answer(10).Should().Be("buzz");
+      }
+      
+      [Test]
+      public void fifteen_should_be_fizzbuzz()
+      {
+         target.Answer(15).Should().Be("fizzbuzz");
+      }
+
+      [Test]
+      public void thirty_should_be_fizzbuzz()
+      {
+         target.Answer(30).Should().Be("fizzbuzz");
+      }
+   }
+}
